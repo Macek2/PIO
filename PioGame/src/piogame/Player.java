830a2645d0a13d5929688535f1377b1d3d1f3e63
Macek <139150464+Macek2@users.noreply.github.com>
2023-10-10 16:45:22 +0200

    Update Player.java

--- PioGame/src/piogame/Player.java
+++ PioGame/src/piogame/Player.java
@@ -4,8 +4,21 @@ import java.util.Random;
 
 public class Player{
     private Random answer = new Random();
-        public int guess(){
-            return answer.nextInt(6) + 1;
-        }
+    public int guess(){
+        return answer.nextInt(6) + 1;
+    }
+    
     private String name;    
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        if (name != null && !name.isEmpty()) {
+            this.name = name;
+        } else {
+            System.err.println("Nieprawidłowe imię!");
+        } 
+    }
+    
 }
