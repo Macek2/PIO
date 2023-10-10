e9eb48b015ba71d144ff246061923784d2f5dcae
Macek <139150464+Macek2@users.noreply.github.com>
2023-10-10 16:09:37 +0200

    Add files via upload

---
+++ PioGame/src/piogame/Player.java
@@ -0,0 +1,11 @@
+package piogame;
+
+import java.util.Random;
+
+public class Player{
+    private Random answer = new Random();
+        public int guess(){
+            return answer.nextInt(6) + 1;
+        }
+        
+    }
