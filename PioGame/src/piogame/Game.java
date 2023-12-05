5a1533d6cb4d57def09cb92d0618e208681b06a3
Macek <139150464+Macek2@users.noreply.github.com>
2023-12-05 16:57:50 +0100

    Add files via upload

--- PioGame/src/piogame/Game.java
+++ PioGame/src/piogame/Game.java
@@ -5,35 +5,103 @@ import java.util.List;
 
 public class Game {
 
-    private Player player;
+    private List<Player> players = new ArrayList<>();
     private Random rand = new Random();
+    private int cnt = -1;
 
-    public void addPlayer(Player player){
-        this.player=player;
+    public void addPlayer(Player player) {
+        players.forEach( p ->{
+            if (player.getName().equals(p.getName())){
+                player.setName(player.getName() + cnt);
+                cnt-=1;
+            }
+        });
+    }
+
+    private boolean nameExists(String name) {
+        for (Player player : players) {
+            if (player.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void play(){
         int number;                     //wylosowana liczba
         int guess;                      //propozycja (strzał) gracza
 
+        boolean repeat;
+
         do {
             System.out.println("---------------------");
+
+            repeat = true;
 
             number = rand.nextInt(6) + 1;
             System.out.println("Kostka: " + number);
 
-            guess = player.guess();
-            System.out.println("Gracz " + player.getName() + ": " + guess);
+            for (Player player : players) {
+                guess = player.guess(); // odgadywanie zlecamy obiektowi klasy Player
+                System.out.println("Gracz " + player.getName() + ": " + guess);
 
-            if (number != guess) {
-                System.out.println("PUDŁO!");
+                if (number != guess) {
+                    System.out.println("PUDŁO!");
+                } else {
+                    System.out.println("BRAWO!");
+                    repeat = false;
+                }
             }
-            else {
-                System.out.println("BRAWO!");
-            }
+        } while (repeat);
+    }
 
-        } while (number != guess);
+    public void printPlayers(){
+        System.out.println("###Gracze:###");
+        for (Player player : players) {
+            System.out.println(player.getName());
+        }
 
+       // players.forEach(player -> {
+       //     System.out.println(player.getName());
+       // });
+
+       // players.forEach(System.out::println);
+    }
+
+    public void removePlayer(String name){
+//        for (Iterator<Player> it = players.iterator(); it.hasNext();) {
+//            if (it.next().getName().equals(name)) {
+//                it.remove();
+//            }
+//            }
+
+        //for(int i=0; i<players.size(); i++){
+        //     if(players.get(i).getName().equals(name)){
+        //        players.remove(i);
+        //    }
+        //}
+
+        // nie stosujemy pętli foreach do usuwania danych z listy
+
+        players.removeIf((Player player) -> player.getName().equals(name));
     }
 
 }
+
+//do {
+//    System.out.println("---------------------");
+
+//        number = rand.nextInt(6) + 1;
+//        System.out.println("Kostka: " + number);
+
+//        guess = player.guess();
+//        System.out.println("Gracz " + player.getName() + ": " + guess);
+
+//        if (number != guess) {
+//        System.out.println("PUDŁO!");
+//        }
+//        else {
+//        System.out.println("BRAWO!");
+//        }
+
+//        } while (number != guess);
--- PioGame/src/piogame/PioGame.java
+++ PioGame/src/piogame/PioGame.java
@@ -15,25 +15,34 @@ import java.util.Random;
 public class PioGame {
     public static void main(String[] args) {
 
-        ArrayList <String> list = new ArrayList();
+        //JAK UZYWAC LISTY:
+        //ArrayList <String> list = new ArrayList();
         //list.add(1); zamiana na list.add(new Integer(1))
-        list.add("jeden");
-        list.add("dwa");
-        list.add("trzy");
-        System.out.println(list);
+        //list.add("jeden");
+        //list.add("dwa");
+        //list.add("trzy");
+        //System.out.println(list);
 
-        for (int i =0; i < list.size(); i++){
-            System.out.println(list.get(i));
-        }
+        //for (int i =0; i < list.size(); i++){
+           // System.out.println(list.get(i));
+        //}
 
+        //INNE METODY WYWOŁANIA GRACZA:
         //Player player = new Player("Piotr");
         // player.setName("Piotr");
         //PlayerHuman player = new PlayerHuman("Piotr");
         //Player player = new PlayerComp("Bot_Varus");
 
-        //Game game = new Game();
-        //game.addPlayer(new PlayerComp("Bot_Varus"));
-        //game.play();
+        Game game = new Game();
+        game.addPlayer(new PlayerComp("Bot_Varus"));
+        game.addPlayer(new PlayerComp("Bot_Blitzcrank"));
+        game.addPlayer(new PlayerComp("Bot_Varus"));
+        game.addPlayer(new PlayerComp("Bot_Blitzcrank"));
+
+        game.printPlayers();
+        game.removePlayer("Bot_Blitzcrank");
+
+        game.play();
     }
 }
 
